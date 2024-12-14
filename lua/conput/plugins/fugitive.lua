e844119aae6c4b9bbc6757c1916bd8713571e2bb
putra <putra@web8.is>
2024-12-14 18:51:00 +0700

    refactor: change keymap

--- lua/conput/plugins/fugitive.lua
+++ lua/conput/plugins/fugitive.lua
@@ -16,7 +16,7 @@ return {
 
 				local bufnr = vim.api.nvim_get_current_buf()
 				local opts = { buffer = bufnr, remap = false }
-				vim.keymap.set("n", "<leader>pu", function()
+				vim.keymap.set("n", "<leader>gp", function()
 					vim.cmd.Git("push")
 				end, opts)
 
