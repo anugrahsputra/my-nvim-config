6ddf9d41492a5b6710790ac5c615e6a6021b2c92
anugrahsputra <anugrahsputra@gmail.com>
2025-04-17 16:37:38 +0700

    add new plugins

--- lua/conput/plugins/render_markdown.lua
+++ lua/conput/plugins/render_markdown.lua
@@ -1,8 +1,8 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
+	opts = {},
+	dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
 	config = function()
 		require("render-markdown").setup({})
 	end,
-	opts = {},
-	dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
 }
