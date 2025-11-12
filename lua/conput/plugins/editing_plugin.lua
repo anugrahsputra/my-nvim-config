9f601f6b7843ebacca891febb2d5e394e490d3bc
anugrahsputra <anugrahsputra@gmail.com>
2025-11-13 01:46:18 +0700

    Refactor plugin configuration: comment out codeium setup and enhance neoscroll options

--- lua/conput/plugins/editing_plugin.lua
+++ lua/conput/plugins/editing_plugin.lua
@@ -88,27 +88,77 @@ return {
 
 	-- codeium
 
+	-- {
+	-- 	"Exafunction/codeium.nvim",
+	-- 	dependencies = {
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		"hrsh7th/nvim-cmp",
+	-- 	},
+	-- 	config = function()
+	-- 		require("codeium").setup({
+	-- 			enable_cmp_source = false,
+	-- 			virtual_text = {
+	-- 				enabled = true,
+	-- 				map_keys = true,
+	-- 				key_bindings = {
+	-- 					accept = "<C-y>",
+	-- 					accept_word = false,
+	-- 					accept_line = false,
+	-- 					clear = false,
+	-- 					next = "<M-]>",
+	-- 					prev = "<M-[>",
+	-- 				},
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
+
+	-- neoscroll
 	{
-		"Exafunction/codeium.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"hrsh7th/nvim-cmp",
-		},
+		"karb94/neoscroll.nvim",
+		opts = {},
 		config = function()
-			require("codeium").setup({
-				enable_cmp_source = false,
-				virtual_text = {
-					enabled = true,
-					map_keys = true,
-					key_bindings = {
-						accept = "<C-y>",
-						accept_word = false,
-						accept_line = false,
-						clear = false,
-						next = "<M-]>",
-						prev = "<M-[>",
-					},
+			require("neoscroll").setup({
+				hide_cursor = true, -- Hide cursor while scrolling
+				stop_eof = true, -- Stop at <EOF> when scrolling downwards
+				respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+				cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+				duration_multiplier = 0.5, -- Global duration multiplier
+				easing = "linear", -- Default easing function
+				pre_hook = nil, -- Function to run before the scrolling animation starts
+				post_hook = nil, -- Function to run after the scrolling animation ends
+				performance_mode = false, -- Disable "Performance Mode" on all buffers.
+				ignored_events = { -- Events ignored while scrolling
+					"WinScrolled",
+					"CursorMoved",
 				},
+			})
+		end,
+	},
+
+	-- Comment.nvim
+	{
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end,
+	},
+	-- supermaven
+	{
+		"supermaven-inc/supermaven-nvim",
+		config = function()
+			require("supermaven-nvim").setup({
+				keymaps = {
+					accept_suggestion = "<C-y>",
+					clear_suggestion = "<C-]>",
+					accept_word = "<C-i>",
+				},
+				log_level = "off", -- set to "off" to disable logging completely
+				disable_inline_completion = false, -- disables inline completion for use with cmp
+				disable_keymaps = false, -- disables built in keymaps for more manual control
+				condition = function()
+					return false
+				end,
 			})
 		end,
 	},
