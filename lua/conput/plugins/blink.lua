aedd7339c8c17cd9b9e238339ca65a0fba517a72
anugrahsputra <anugrahsputra@gmail.com>
2026-05-07 12:44:21 +0700

    keybind ughh

--- lua/conput/plugins/blink.lua
+++ lua/conput/plugins/blink.lua
@@ -6,6 +6,10 @@ return {
 		keymap = {
 			preset = "default",
 			["<CR>"] = { "select_and_accept", "fallback" },
+			["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+			["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+			["<C-n>"] = { "select_next", "fallback" },
+			["<C-p>"] = { "select_prev", "fallback" },
 		},
 		appearance = {
 			use_nvim_cmp_as_default = false,
--- lua/conput/plugins/supermaven.lua
+++ lua/conput/plugins/supermaven.lua
@@ -2,17 +2,22 @@ return {
 	"supermaven-inc/supermaven-nvim",
 	config = function()
 		require("supermaven-nvim").setup({
-			keymaps = {
-				accept_suggestion = "<C-y>",
-				clear_suggestion = "<C-]>",
-				accept_word = "<C-i>",
-			},
 			log_level = "off", -- set to "off" to disable logging completely
 			disable_inline_completion = false, -- disables inline completion for use with cmp
-			disable_keymaps = false, -- disables built in keymaps for more manual control
+			disable_keymaps = true, -- disables built in keymaps for more manual control
 			condition = function()
-				return false
+				return true
 			end,
 		})
+
+		vim.keymap.set("i", "<C-y>", function()
+			require("supermaven-nvim.completion_preview").on_accept_suggestion()
+		end)
+		vim.keymap.set("i", "<C-]>", function()
+			require("supermaven-nvim.completion_preview").on_dispose_inlay()
+		end)
+		vim.keymap.set("i", "<C-j>", function()
+			require("supermaven-nvim.completion_preview").on_accept_suggestion_word()
+		end)
 	end,
 }
